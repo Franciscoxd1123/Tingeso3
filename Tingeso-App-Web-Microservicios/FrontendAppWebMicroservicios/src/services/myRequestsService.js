9b73733a092244492e83e89d708fe5ee373c20bd
Francisco Cea <francisco.cea@usach.cl>
2024-12-20 22:32:38 -0300

    Cambio de carpeta

--- Tingeso-App-Web-Microservicios/FrontendAppWebMicroservicios/src/services/myRequestsService.js
+++
@@ -1,8 +0,0 @@
-import httpClient from "../http-common";
-
-// Obtener todas las solicitudes de un cliente por su RUT
-const getRequestsByRut = (clientRut) => {
-    return httpClient.get(`/app/micro/myrequests/${clientRut}`);
-}
-
-export default { getRequestsByRut };
